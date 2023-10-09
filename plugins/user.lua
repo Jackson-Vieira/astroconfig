f343ac76b8e63de2c73c1ecb9bcf502533a47ed2
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-09 09:39:35 -0300

    add new plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,28 @@
 return {
- { "andweeb/presence.nvim", lazy=false}, 
+ { "andweeb/presence.nvim", lazy=false},
+ {
+  "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+ },
  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
  { "ThePrimeagen/vim-be-good", cmd = { "VimBeGood" }},
+ {
+   "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+     "nvim-lua/plenary.nvim",
+     "nvim-treesitter/nvim-treesitter",
+    },
+    cmd= "Refactor",
+    config = function()
+      require("refactoring").setup()
+    end,
+ },
  {
    "zbirenbaum/copilot.lua",
    cmd = "Copilot",
